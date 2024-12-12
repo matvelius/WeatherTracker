4969cc2e6f46d8d9ea6f35034a6d5a1717db9e74
Matvey Kostukovsky <email@matveycodes.com>
2024-12-11 20:36:53 -0800

    Initial Commit

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Matvey Kostukovsky on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
